883b5c0bfe8ed9a6d91689a15bd7ee2574c79e8a
Oskey2277 <oskarnepal2277@gmail.com>
2025-04-10 00:47:45 +0545

    completed

---
+++ Week1/src/week1/Diamond.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class Diamond {
+
+	public static void main(String[] args) {
+		  System.out.println("   *   ");
+	      System.out.println("  ***  ");
+	      System.out.println(" ***** ");
+	      System.out.println("*******");
+	      System.out.println(" ***** ");
+	      System.out.println("  ***  ");
+	      System.out.println("   *   ");
+
+	}
+
+}
+
+  
